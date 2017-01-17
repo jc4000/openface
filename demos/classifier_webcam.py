999c310713d83fa220045010aaa77289345d4720
Jayanta Choudhury <Jayanta Choudhury>
2017-01-17 03:13:40 -0800

    Change in the demo/classifier_webcam.py

--- demos/classifier_webcam.py
+++ demos/classifier_webcam.py
@@ -136,6 +136,7 @@ def infer(img, args):
 
 if __name__ == '__main__':
 
+    foundFlg = False
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--dlibFacePredictor',
@@ -184,24 +185,27 @@ if __name__ == '__main__':
     confidenceList = []
     while True:
         ret, frame = video_capture.read()
-        persons, confidences = infer(frame, args)
-        print "P: " + str(persons) + " C: " + str(confidences)
-        try:
-            # append with two floating point precision
-            confidenceList.append('%.2f' % confidences[0])
-        except:
-            # If there is no face detected, confidences matrix will be empty.
-            # We can simply ignore it.
-            pass
-
-        for i, c in enumerate(confidences):
-            if c <= args.threshold:  # 0.5 is kept as threshold for known face.
-                persons[i] = "_unknown"
-
-                # Print the person name and conf value on the frame
-        cv2.putText(frame, "P: {} C: {}".format(persons, confidences),
-                    (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-        cv2.imshow('', frame)
+        if not foundFlg:
+            persons, confidences = infer(frame, args)
+            print "P: " + str(persons) + " C: " + str(confidences)
+            try:
+                # append with two floating point precision
+                confidenceList.append('%.2f' % confidences[0])
+            except:
+                # If there is no face detected, confidences matrix will be empty.
+                # We can simply ignore it.
+                pass
+    
+            for i, c in enumerate(confidences):
+                if c <= args.threshold:  # 0.5 is kept as threshold for known face.
+                    persons[i] = "_unknown"
+                else:
+    
+                    # Print the person name and conf value on the frame
+            cv2.putText(frame, "P: {} C: {}".format(persons, confidences),
+                        (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            cv2.imshow('', frame)
+            foundFlg = True
         # quit the program on the press of key 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
